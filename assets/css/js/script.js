e59bc1a97005efe5332bd1cfaebcc3fa0e8a35df
appropriation1234 <tiago.toledo.oliveira@gmail.com>
2023-03-30 20:50:03 -0300

    Animacao

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,32 +1,43 @@
 console.log("calcular orcamento")
 
 // Manipulção do DOM
-let inputELO = document.querySelector("ELO")
-let inputJS = document.querySelector("js")
+let inputELOAtual = document.querySelector("#ELOAtual")
+let inputELOFinal = document.querySelector("#ELOFinal")
 let inputDuoboost = document.querySelector("#Duoboost-sim")
 let inputDuoboostNao = document.querySelector("#Duoboost-nao")
 let inputPrazo = document.querySelector("#prazo")
 
-inputELO.addEventListener("change", atualizarOrcamento)
-inputJs.addEventListener("change", atualizarOrcamento)
+inputELOAtual.addEventListener("change", atualizarOrcamento)
+inputELOFinal.addEventListener("change", atualizarOrcamento)
 inputDuoboost.addEventListener("change", atualizarOrcamento)
 inputDuoboostNao.addEventListener("change", atualizarOrcamento)
 inputPrazo.addEventListener("change", atualizarOrcamento)
 
 function atualizarOrcamento(){
-    let preco = ELO.value * 100
-    let js = inputJS.checked
+    let preco = inputELOAtual.value * 100
     let Duoboost = inputDuoboost.checked
     let prazo = inputPrazo.value
 
     let labelPrazo = document.querySelector("label[for=prazo]")
     labelPrazo.innerHTML = `Prazo (${prazo} dias)`
 
-    if(js) preco *= 1.1
-    if(Duoboost) preco += ELO.value * 30/100
+    if(Duoboost) preco += inputELOAtual.value * 30
     let taxaDeUrgencia = 1 - prazo*.05
     preco *= 1 + taxaDeUrgencia
 
 
     document.querySelector("#preco").innerHTML = "R$ " + preco.toFixed(2)
+
 }
+
+
+function mostrarTexto(id) {
+    var texto = document.getElementById(id);
+    texto.style.display = "block";
+}
+
+
+function esconderTexto(id) {
+    var texto = document.getElementById(id);
+    texto.style.display = "none";
+  }
